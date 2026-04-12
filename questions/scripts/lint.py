95d7548eb6dfe1f3ad116ce108759e31b5b96b03
Quasar-Kim <vhfflrhs128@gmail.com>
2026-04-13 00:14:59 +0900

    (questions) Refactor build scripts

--- questions/scripts/lint.py
+++ questions/scripts/lint.py
@@ -8,6 +8,31 @@
 # ]
 # ///
 
+"""
+아카이브의 문제들이 실수 없이 작성되었는지 검사하기 위한 스크립트.
+
+아카이브의 유지관리성을 위해서는 문제 작성 과정에서 흔하게 발생하는 실수들을 방지해야 한다.
+이러한 실수들은 파일 형식의 올바름과는 관계없이 발생할 수 있는데,
+예를 들어 아무 곳에도 참조되지 않는 첨부 파일이 특정 문제의 디렉토리에 포함되는 경우가 대표적이다.
+이 스크립트는 이러한 실수들의 일부를 자동화된 방식으로 빠르게 잡아내기 위한 목적으로 작성되었다.
+
+이 스크립트는 mistune 등 외부 패키지에 의존하므로 `uv` 등의 도구를 이용해서 다음과 같이
+실행되어야 한다:
+
+```
+uv run --script lint.py
+```
+
+또는 맨 윗줄에 자동으로 `uv`를 호출하는 shebang이 포함되어 있다는 것을 이용해 바로 실행하는 것도
+가능하다:
+
+```
+./scripts/lint.py
+```
+
+이 스크립트는 `questions` 디렉토리 안의 어느 위치에서 실행해도 정상적으로 동작한다.
+"""
+
 from __future__ import annotations
 from pathlib import Path
 from typing import Optional, Protocol
@@ -15,179 +40,63 @@ import sys
 import re
 from termcolor import colored
 from Question import (
-    Question, 
-    META_REQUIRED_KEYS, 
-    META_FILENAME, 
-    QUESTION_FILENAME, 
-    LONG_ANSWER_FILENAME, 
-    QuestionLoadingError
+    Question,
+    META_REQUIRED_KEYS,
+    META_FILENAME,
+    QUESTION_FILENAME,
+    LONG_ANSWER_FILENAME,
+    QuestionLoadingError,
 )
 
 
-SRC_DIR = Path(__file__, '../../src').resolve()
-VALID_SUBJECTS = set(['cs', 'math', 'chem', 'bio', 'phys', 'unknown'])
+def main():
+    src_dir = Path(__file__, "../../src").resolve()
+
+    # src 디렉토리 안의 모든 디렉토리에 대해서 `check_all()` 호출.
+    failed = False
+    count = 0
+    for p in src_dir.iterdir():
+        if not p.is_dir():
+            continue
+        ok = check_all(p)
+        if ok:
+            count += 1
+        elif not failed:
+            failed = True
+
+    # 단 한 문제에서라도 문제가 발생했을 시 종료 코드 설정.
+    if failed:
+        sys.exit(1)
+    else:
+        print(f"Checked {count} questions. OK.")
 
 
-class LintError(Exception):
-    def __init__(self, message, rule=None, question=None):
-        super().__init__(message)
-        self.rule = rule
-        self.question = question
-        
+def check_all(question_dir: Path) -> bool:
+    """
+    `question_dir` 디렉토리에 대해 정의된 모든 체크들을 실행한다.
+    모든 체크를 통과했을 시 `True`를, 한 체크라도 실패했을시 `False`를 반환한다.
+    """
 
-class Check(Protocol):
-    def __call__(self, q: Question) -> CheckResult:
-        ...
+    checks = [
+        CheckIdFormat(),
+        CheckDirname(),
+        CheckExtraKeys(),
+        CheckYearRange(),
+        CheckSubject(),
+        CheckNoEmptyQuestion(),
+        CheckQuestionNoInvalidRef(),
+        CheckLongAnswerNoInvalidRef(),
+        CheckNoUnusedAsset(),
+    ]
 
-    def rule_name(self) -> str:
-        ...
-
-    def default_msg(self) -> str:
-        ...
-
-class CheckResult:
-    def __init__(self, q: Question, check: Check, ok: bool, msg: Optional[str] = None):
-        self.q = q
-        self.check = check
-        self.ok = ok
-        self.msg = msg
-
-    def output(self) -> str:
-        status = colored('OK', 'green') if self.ok else colored('WARN', 'yellow')
-        msg = self.check.default_msg() if self.msg is None else self.msg
-        rule_name = colored('(' + self.check.rule_name() + ')', 'dark_grey')
-        return f'{status} {msg} {rule_name}'
-        
-
-class CheckIdFormat(Check):
-    def __call__(self, q: Question) -> CheckResult:
-        year = q.meta['year']
-        subject = q.meta['subject']
-        id_regex = re.compile(f'{year}-{subject}-\\d+')
-        return CheckResult(q, self, id_regex.match(q.meta['id']) is not None)
-
-    def rule_name(self) -> str:
-        return 'id-format'
-    
-    def default_msg(self) -> str:
-        return 'ID format should be in {year}-{subject}-{digit} format'
-
-class CheckDirname(Check):
-    def __call__(self, q: Question) -> CheckResult:
-        return CheckResult(q, self, q.dir.parts[-1] == q.meta['id'])
-
-    def rule_name(self):
-        return 'dirname'
-    
-    def default_msg(self):
-        return 'directory name should match question ID'
-    
-class CheckExtraKeys(Check):
-    def __call__(self, q):
-        extra_keys = set(q.meta.keys()) - META_REQUIRED_KEYS
-        if len(extra_keys) > 0:
-            extra_keys_str = ', '.join(extra_keys)
-            return CheckResult(q, self, False, self.default_msg() + f'. Extra keys found: {extra_keys_str}')
-        return CheckResult(q, self,  True)
-
-    def rule_name(self):
-        return 'extra-keys'
-    
-    def default_msg(self):
-        return f'{META_FILENAME} should not contain extra keys'
-
-class CheckYearRange(Check):
-    def __call__(self, q):
-        return CheckResult(q, self, 2000 <= q.meta['year'] < 3000)
-
-    def rule_name(self):
-        return 'year-range'
-    
-    def default_msg(self):
-        return 'year should be in range [2000, 3000)'
-
-class CheckSubject(Check):
-    def __call__(self, q):
-        return CheckResult(q, self, q.meta['subject'] in VALID_SUBJECTS)
-
-    def rule_name(self):
-        return 'subject'
-
-    def default_msg(self):
-        subjects_str = ', '.join(VALID_SUBJECTS)
-        return f'subject should be one of known types: {subjects_str}'
-
-class CheckNoEmptyQuestion(Check):
-    def __call__(self, q):
-        return CheckResult(q, self, len(q.question_text.strip()) > 0)
-
-    def rule_name(self):
-        return 'no-empty-question'
-    
-    def default_msg(self):
-        return f'{QUESTION_FILENAME} should not be empty'
-    
-class CheckQuestionNoInvalidRef(Check):
-    def __call__(self, q):
-        invalid_refs = q.question_refs - q.assets
-        if len(invalid_refs) > 0:
-            invalid_refs_str = ', '.join(str(p) for p in invalid_refs)
-            return CheckResult(q, self, False, f'{self.default_msg()}: {invalid_refs_str}')
-        return CheckResult(q, self, True)
-    
-    def rule_name(self):
-        return 'question-no-invalid-ref'
-    
-    def default_msg(self):
-        return f'{QUESTION_FILENAME} should not have invalid reference'
-
-class CheckLongAnswerNoInvalidRef(Check):
-    def __call__(self, q):
-        invalid_refs = q.long_answer_refs - q.assets
-        if len(invalid_refs) > 0:
-            invalid_refs_str = ', '.join(str(p) for p in invalid_refs)
-            return CheckResult(q, self, False, f'{self.default_msg()}: {invalid_refs_str}')
-        return CheckResult(q, self, True)
-    
-    def rule_name(self):
-        return 'long-answer-no-invalid-ref'
-    
-    def default_msg(self):
-        return f'{LONG_ANSWER_FILENAME} should not have invalid reference'
-    
-class CheckNoUnusedAsset(Check):
-    def __call__(self, q):
-        unused_assets = q.assets - q.refs
-        if len(unused_assets) > 0:
-            unused_assets_str = ', '.join(str(p) for p in unused_assets)
-            return CheckResult(q, self, False, f'{self.default_msg()}: {unused_assets_str}')
-        return CheckResult(q, self, True)
-    
-    def rule_name(self):
-        return 'no-unused-asset'
-    
-    def default_msg(self):
-        return f'all assets must be referenced either by {QUESTION_FILENAME} or {LONG_ANSWER_FILENAME}'
-
-checks = [
-    CheckIdFormat(),
-    CheckDirname(),
-    CheckExtraKeys(),
-    CheckYearRange(),
-    CheckSubject(),
-    CheckNoEmptyQuestion(),
-    CheckQuestionNoInvalidRef(),
-    CheckLongAnswerNoInvalidRef(),
-    CheckNoUnusedAsset(),
-]
-
-def check_all(path: Path) -> bool:
     try:
-        q = Question(path)
+        q = Question(question_dir)
     except QuestionLoadingError as e:
-        print(colored('ERR', 'red'), 'failed to load question:', str(e))
+        print(colored("ERR", "red"), "failed to load question:", str(e))
         return False
-    
+
+    # 각 체크들은 어느 정도 독립적으므로, 번거로움을 줄이기 위해 모든 체크를 실행한 후
+    # 발생한 오류들을 한번에 모아 출력.
     failures = []
     for c in checks:
         result = c(q)
@@ -197,19 +106,302 @@ def check_all(path: Path) -> bool:
     if len(failures) > 0:
         print(q.dir)
         for f in failures:
-            print('  ' + f.output())
+            print("  " + f.output())
         return False
 
     return True
-    
 
-failed = False
-for p in SRC_DIR.iterdir():
-    if not p.is_dir():
-        continue
-    ok = check_all(p)
-    if not ok and not failed:
-        failed = True
 
-if failed:
-    sys.exit(1)
+class Check(Protocol):
+    """
+    각 체크 항목을 나타내는 프로토콜.
+    """
+
+    def __call__(self, q: Question) -> CheckResult:
+        """
+        문제 `q` 에 대해서 체크를 실행한다.
+        """
+        ...
+
+    def rule_name(self) -> str:
+        """
+        체크 항목의 이름을 반환하는 함수. 결과 출력시 유용히 사용된다.
+        """
+        ...
+
+    def default_msg(self) -> str:
+        """
+        체크 항목의 기본 오류 메시지. 결과 출력시 설정된 오류 메시지가 별도로 없다면 해당 메시지가
+        출력된다.
+        """
+        ...
+
+
+class CheckResult:
+    """
+    체크 항목의 결과를 나타내는 클래스.
+    """
+
+    def __init__(self, q: Question, check: Check, ok: bool, msg: Optional[str] = None):
+        self.q = q
+        self.check = check
+        self.ok = ok
+        self.msg = msg
+
+    def output(self) -> str:
+        """
+        터미널에 출력하기 위한 용도로 스타일링된 결과 문자열을 반환하는 함수.
+        """
+
+        status = colored("OK", "green") if self.ok else colored("WARN", "yellow")
+        msg = self.check.default_msg() if self.msg is None else self.msg
+        rule_name = colored("(" + self.check.rule_name() + ")", "dark_grey")
+        return f"{status} {msg} {rule_name}"
+
+
+class CheckIdFormat(Check):
+    """
+    `meta.yml` 의 `id` 필드가 `{year}-{subject}-{digit}` 포멧인지 확인한다.
+
+    `year` 부분은 `year` 필드와, `subject` 부분은 `subject` 필드와 일치해야 하며,
+    `digit` 부분에는 임의의 숫자가 들어갈 수 있다.
+
+    올바른 예시:
+
+    .. code-block:: yaml
+
+        id: 2024-cs-1
+        year: 2024
+        subject: cs
+
+    올바르지 않은 예시:
+
+    .. code-block:: yaml
+
+        id: 2023-math-x
+        year: 2024
+        subject: cs
+    """
+
+    def __call__(self, q: Question) -> CheckResult:
+        year = q.meta["year"]
+        subject = q.meta["subject"]
+        id_regex = re.compile(f"{year}-{subject}-\\d+")
+        return CheckResult(q, self, id_regex.match(q.meta["id"]) is not None)
+
+    def rule_name(self) -> str:
+        return "id-format"
+
+    def default_msg(self) -> str:
+        return "ID format should be in {year}-{subject}-{digit} format"
+
+
+class CheckDirname(Check):
+    """
+    문제 디렉토리 이름이 `meta.yml` 의 `id` 필드와 일치하는지 확인한다.
+    """
+
+    def __call__(self, q: Question) -> CheckResult:
+        return CheckResult(q, self, q.dir.parts[-1] == q.meta["id"])
+
+    def rule_name(self):
+        return "dirname"
+
+    def default_msg(self):
+        return "directory name should match question ID"
+
+
+class CheckExtraKeys(Check):
+    """
+    `meta.yml` 에 알려지지 않은 키가 포함되어 있는지 확인한다.
+
+    만약 새로운 키를 추가해야 할 경우, 해당 체크는 그대로 두고 `Question.py` 모듈의
+    `META_REQUIRED_KEYS` 에 키를 추가하면 된다.
+
+    올바른 예시:
+
+    .. code-block:: yaml
+
+        id: 2024-cs-1
+        year: 2024
+        subject: cs
+        short_answer: '16'
+
+    올바르지 않은 예시:
+
+    .. code-block:: yaml
+
+        priority: high  # 알려지지 않은 키
+        id: 2024-cs-1
+        year: 2024
+        subject: cs
+        short_answer: '16'
+    """
+
+    def __call__(self, q):
+        extra_keys = set(q.meta.keys()) - META_REQUIRED_KEYS
+        if len(extra_keys) > 0:
+            extra_keys_str = ", ".join(extra_keys)
+            return CheckResult(
+                q,
+                self,
+                False,
+                self.default_msg() + f". Extra keys found: {extra_keys_str}",
+            )
+        return CheckResult(q, self, True)
+
+    def rule_name(self):
+        return "extra-keys"
+
+    def default_msg(self):
+        return f"{META_FILENAME} should not contain extra keys"
+
+
+class CheckYearRange(Check):
+    """
+    `meta.yml` 의 `year` 필드의 값이 적절한 구간의 값 (2000 이상 3000 미만) 인지 확인한다.
+    """
+
+    def __call__(self, q):
+        return CheckResult(q, self, 2000 <= q.meta["year"] < 3000)
+
+    def rule_name(self):
+        return "year-range"
+
+    def default_msg(self):
+        return "year should be in range [2000, 3000)"
+
+
+class CheckSubject(Check):
+    """
+    `meta.yml` 의 `subject` 필드가 알려진 값 중 하나인지 확인한다.
+
+    현재 허용된 값으로는 컴퓨터공학 (`cs`), 수학 (`math`), 화학 (`chem`), 생명공학 (`bio`),
+    물리학 (`phys`), 그리고 미분류 (`unknown`) 이 있다.
+    """
+
+    valid_subjects = set(["cs", "math", "chem", "bio", "phys", "unknown"])
+
+    def __call__(self, q):
+        return CheckResult(q, self, q.meta["subject"] in self.valid_subjects)
+
+    def rule_name(self):
+        return "subject"
+
+    def default_msg(self):
+        subjects_str = ", ".join(self.valid_subjects)
+        return f"subject should be one of known types: {subjects_str}"
+
+
+class CheckNoEmptyQuestion(Check):
+    """
+    `question.md` 파일의 내용이 없는 경우를 잡아낸다.
+    """
+
+    def __call__(self, q):
+        return CheckResult(q, self, len(q.question_text.strip()) > 0)
+
+    def rule_name(self):
+        return "no-empty-question"
+
+    def default_msg(self):
+        return f"{QUESTION_FILENAME} should not be empty"
+
+
+class CheckQuestionNoInvalidRef(Check):
+    """
+    `question.md` 파일이 참조하는 첨부파일들이 모두 올바른지 확인한다.
+
+    마크다운 파일은 첨부파일을 사진 또는 링크의 형태로 참조할 수 있는데, 이 참조가 가리키는 파일이
+    동일 디렉토리에 존재하지 않는 경우를 잡아낸다.
+
+    아래 예시에서는 다음과 같은 파일들이 디렉토리에 포함되어 있다고 상정한다.
+
+    .. code-block:: text
+        - meta.yml
+        - question.md
+        - answer.md
+        - tree.png
+        - extra.txt
+
+    올바른 예시:
+
+    .. code-block:: markdown
+
+        아래 트리를 보아라:
+        ![트리](./tree.png)
+
+        또는 [이 파일](./extra.txt)을 보아라.
+
+    올바르지 않은 예시:
+
+    .. code-block:: markdown
+
+        아래 트리를 보아라:
+        ![트리](./the-tree.png)
+
+        또는 [이 파일](./this-file.txt)을 보아라.
+    """
+
+    def __call__(self, q):
+        invalid_refs = q.question_refs - q.assets
+        if len(invalid_refs) > 0:
+            invalid_refs_str = ", ".join(str(p) for p in invalid_refs)
+            return CheckResult(
+                q, self, False, f"{self.default_msg()}: {invalid_refs_str}"
+            )
+        return CheckResult(q, self, True)
+
+    def rule_name(self):
+        return "question-no-invalid-ref"
+
+    def default_msg(self):
+        return f"{QUESTION_FILENAME} should not have invalid reference"
+
+
+class CheckLongAnswerNoInvalidRef(Check):
+    """
+    `answer.md` 파일이 참조하는 첨부파일들이 모두 올바른지 확인한다.
+
+    자세한 설명은 `CheckQuestionNoInvalidRef` 클래스의 docstring 을 참조하여라.
+    """
+
+    def __call__(self, q):
+        invalid_refs = q.long_answer_refs - q.assets
+        if len(invalid_refs) > 0:
+            invalid_refs_str = ", ".join(str(p) for p in invalid_refs)
+            return CheckResult(
+                q, self, False, f"{self.default_msg()}: {invalid_refs_str}"
+            )
+        return CheckResult(q, self, True)
+
+    def rule_name(self):
+        return "long-answer-no-invalid-ref"
+
+    def default_msg(self):
+        return f"{LONG_ANSWER_FILENAME} should not have invalid reference"
+
+
+class CheckNoUnusedAsset(Check):
+    """
+    `question.md` 및 `answer.md` 가 참조하지 않는 첨부파일이 존재하는지 확인한다.
+    """
+
+    def __call__(self, q):
+        unused_assets = q.assets - q.refs
+        if len(unused_assets) > 0:
+            unused_assets_str = ", ".join(str(p) for p in unused_assets)
+            return CheckResult(
+                q, self, False, f"{self.default_msg()}: {unused_assets_str}"
+            )
+        return CheckResult(q, self, True)
+
+    def rule_name(self):
+        return "no-unused-asset"
+
+    def default_msg(self):
+        return f"all assets must be referenced either by {QUESTION_FILENAME} or {LONG_ANSWER_FILENAME}"
+
+
+if __name__ == "__main__":
+    main()
--- questions/scripts/make_db.py
+++ questions/scripts/make_db.py
@@ -8,84 +8,167 @@
 # ]
 # ///
 
+"""
+아카이브를 sqlite 데이터베이스 파일로 변환하기 위한 스크립트.
+
+이 스크립트는 mistune 등 외부 패키지에 의존하므로 `uv` 등의 도구를 이용해서 다음과 같이
+실행되어야 한다:
+
+```
+uv run --script make_db.py
+```
+
+또는 맨 윗줄에 자동으로 `uv`를 호출하는 shebang이 포함되어 있다는 것을 이용해 바로 실행하는 것도
+가능하다:
+
+```
+./scripts/make_db.py
+```
+
+이 스크립트는 `questions` 디렉토리 안의 어느 위치에서 실행해도 정상적으로 동작한다.
+"""
+
 from __future__ import annotations
 import sqlite3
 from pathlib import Path
-from Question import (
-    Question
-)
+from typing import cast
+from Question import Question
 from urllib.parse import urlsplit
 import mistune
 from mistune.renderers.markdown import MarkdownRenderer
 
+
+OUTPUT_NAME = "questions.db"
+
+# == 타입 참조들:
+
+# 데이터베이스의 `question` 테이블의 각 행을 나타내는 튜플.
+# (문제 ID, 연도, 과목, 문제 텍스트, 정답, 해설)
+QuestionRow = tuple[str, int, str, str, str, str]
+
+# 첨부파일의 기존 경로(키)와 새로운 경로(값)를 매핑하는 딕셔너리.
+AssetMapping = dict[Path, Path]
+
+
+def main():
+    src_dir = Path(__file__, "../../src").resolve()
+    db_path = Path(__file__, f"../../{OUTPUT_NAME}").resolve()
+
+    # 모든 문제들의 내용과 모든 첨부파일들의 경로 매핑을 불러옴.
+    all_question_rows: list[QuestionRow] = []
+    all_assets: AssetMapping = dict()
+    for question_dir in src_dir.iterdir():
+        if not question_dir.is_dir():
+            continue
+        q = Question(question_dir)
+        question_row, assets = process_question(question_dir, q)
+        all_question_rows.append(question_row)
+        all_assets |= assets
+
+    if db_path.exists():
+        db_path.unlink()
+
+    con = sqlite3.connect(db_path)
+    cur = con.cursor()
+
+    # questions 테이블 생성.
+    cur.execute(
+        "CREATE TABLE questions(id, year, subject, question_text, short_answer, long_answer_text)"
+    )
+    cur.executemany("INSERT INTO questions VALUES(?, ?, ?, ?, ?, ?)", all_question_rows)
+
+    # assets 테이블 생성.
+    cur.execute("CREATE TABLE assets(name, data)")
+    for data_path, asset_path in all_assets.items():
+        with data_path.open("rb") as f:
+            data = f.read()
+            cur.execute("INSERT INTO assets VALUES(?, ?)", (asset_path.name, data))
+
+    con.commit()
+
+    print(
+        (
+            f"Generated {OUTPUT_NAME} with {len(all_question_rows)} questions "
+            f"and {len(all_assets)} assets"
+        )
+    )
+
+
+def process_question(
+    question_dir: Path, q: Question
+) -> tuple[QuestionRow, AssetMapping]:
+    """
+    문제 `q` 를 데이터베이스에 삽입될 수 있는 형태로 변환하는 함수.
+    문제의 모든 첨부파일 참조를 `/assets/{ID}_{filename}` 형태로 변환 후, 테이블 행 형태로 변환된
+    문제와 첨부파일 참조의 매핑을 반환한다.
+
+    첨부파일 참조의 매핑의 키는 기존 경로, 값은 새로운 경로이다.
+    """
+    qid = q.meta["id"]
+
+    # 첨부파일 매핑 생성.
+    # 첨부파일들은 모두 동일한 테이블에 저장되기에 이름 충돌을 방지하기 위해서 고유한 이름이 부여되어야 한다.
+    # 따라서 문제 ID를 접두사로 사용한다.
+    asset_mapping = {k: Path(f"/assets/{qid}_{k.name}") for k in q.assets}
+
+    # 문제 텍스트와 해설의 참조를 매핑을 이용해서 변환.
+    question_text = convert_ref(q.question_text, question_dir, asset_mapping)
+    long_answer_text = convert_ref(q.long_answer_text, question_dir, asset_mapping)
+
+    # 테이블 행 형태의 문제와 첨부파일 매핑을 반환.
+    return (
+        (
+            q.meta["id"],
+            q.meta["year"],
+            q.meta["subject"],
+            question_text,
+            q.meta["short_answer"],
+            long_answer_text,
+        ),
+        asset_mapping,
+    )
+
+
+def convert_ref(
+    markdown_text: str, asset_base_dir: Path, asset_mapping: dict[Path, Path]
+) -> str:
+    """
+    마크다운 텍스트 `markdown_text` 가 참조 (링크, 이미지) 하는 첨부파일들의 링크를 `asset_mapping` 에
+    따라 변환한다. 모든 첨부파일들은 `asset_base_dir` 디렉토리 내부에 존재해야 한다.
+    """
+    convert = mistune.create_markdown(
+        renderer=RefConverter(asset_base_dir, asset_mapping)
+    )
+    return cast(str, convert(markdown_text))
+
+
 class RefConverter(MarkdownRenderer):
+    """
+    마크다운 텍스트가 참조하는 모든 첨부파일 (링크 및 사진) 의 경로를 딕셔너리 `asset_mapping` 에 따라서
+    변환하는 마크다운 프로세서.
+    """
+
     def __init__(self, asset_base_dir, asset_mapping):
         self.asset_base_dir = asset_base_dir
         self.asset_mapping = asset_mapping
 
     def link(self, token, state):
         return super().link(self.replace_asset_ref(token), state)
-    
+
     def image(self, token, state):
         return super().image(self.replace_asset_ref(token), state)
 
     def replace_asset_ref(self, token):
-        url = urlsplit(token['attrs']['url'])
+        url = urlsplit(token["attrs"]["url"])
         if len(url.scheme) > 0:
             return token
-        if url.path.startswith('/assets'):
+        if url.path.startswith("/assets"):
             return token
-        
+
         path = Path(self.asset_base_dir, url.path).resolve()
         new_path = str(self.asset_mapping[path])
-        return dict(token, attrs=dict(token['attrs'], url=new_path))
+        return dict(token, attrs=dict(token["attrs"], url=new_path))
 
 
-SRC_DIR = Path(__file__, '../../src').resolve()
-DB_PATH = Path(__file__, '../../questions.db').resolve()
-
-def convert_ref(text, asset_name_mapping, base_dir):
-    convert = mistune.create_markdown(renderer=RefConverter(base_dir, asset_name_mapping))
-    return convert(text)
-
-def process_question(p):
-    q = Question(p)
-    qid = q.meta['id']
-
-    # assign each assets new path
-    asset_mapping = {k: Path(f'/assets/{qid}_{k.name}') for k in q.assets}
-
-    # convert all references (image, link) to /assets/{uuid}.{ext} format
-    question_text = convert_ref(q.question_text, asset_mapping, p)
-    long_answer_text = convert_ref(q.long_answer_text, asset_mapping, p)
-    return (
-        (q.meta['id'], q.meta['year'], q.meta['subject'], question_text, q.meta['short_answer'], long_answer_text), 
-        {v: k for k, v in asset_mapping.items()}
-    )
-
-all_questions = []
-all_assets = dict()
-for p in SRC_DIR.iterdir():
-    if not p.is_dir():
-        continue
-
-    question, assets = process_question(p)
-    all_questions.append(question)
-    all_assets |= assets
-    
-# dump loaded questions as sqlite DB
-if DB_PATH.exists():
-    DB_PATH.unlink()
-
-con = sqlite3.connect(DB_PATH)
-cur = con.cursor()
-cur.execute('CREATE TABLE questions(id, year, subject, question_text, short_answer, long_answer_text)')
-cur.executemany('INSERT INTO questions VALUES(?, ?, ?, ?, ?, ?)', all_questions)
-
-cur.execute('CREATE TABLE assets(name, data)')
-for asset_path, data_path in all_assets.items():
-    with data_path.open('rb') as f:
-        data = f.read()
-        cur.execute('INSERT INTO assets VALUES(?, ?)', (asset_path.name, data))
-
-con.commit()
+if __name__ == "__main__":
+    main()
