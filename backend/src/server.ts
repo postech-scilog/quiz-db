753c8c49c4d491ba0425f37e074c76fcb8b13270
Quasar-Kim <vhfflrhs128@gmail.com>
2026-04-23 23:44:16 +0900

    (backend) Disable CSP to allow loading image from 'blob:` URL

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,11 +11,15 @@ const logger = pino();
 
 const app = express();
 app.disable("x-powered-by"); // fingerprinting 감소를 위해
-app.use(helmet());
+app.use(
+	helmet({
+		contentSecurityPolicy: false,
+	}),
+);
 app.use(pinoHttp({ logger }));
 app.use(express.static("public")); // public 디렉토리의 파일 서빙, 404등 오류 발생 시 라우터로 fallthrough
 app.use("/", router);
 app.use(errorHandler);
 app.listen(PORT, () => {
-  logger.info(`listening on port ${PORT}`);
+	logger.info(`listening on port ${PORT}`);
 });
