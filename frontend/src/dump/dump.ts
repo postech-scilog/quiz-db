875db2ce53eaa12e9d6338d2777a75484a78be73
Quasar-Kim <vhfflrhs128@gmail.com>
2026-06-01 05:25:56 +0900

    (frontend) Implement dump file generator

---
+++ frontend/src/dump/dump.ts
@@ -0,0 +1,90 @@
+// TODO: 일부 함수가 print.ts와 동일함. 나중에 합쳐야 함.
+
+interface Question {
+	id: string;
+	year: number;
+	subject: string;
+	question_text: string;
+	short_answer: string;
+	long_answer_text: string;
+}
+
+const elems = {
+	status: document.querySelector("#status") as HTMLSpanElement,
+	previewTextArea: document.querySelector(
+		"#dumpPreview",
+	) as HTMLTextAreaElement,
+	downloadButton: document.querySelector("#downloadBtn") as HTMLButtonElement,
+};
+
+async function main() {
+	const params = new URLSearchParams(window.location.search);
+	const subject = params.get("subject");
+	if (!subject || subject?.length === 0) {
+		throw new Error("`subject` paramter is missing from the URL");
+	}
+
+	elems.status.textContent = "문제를 불러오는 중...";
+
+	const questions = await fetchQuestions(subject);
+	const dump = questions.map((q) => ({
+		id: q.id,
+		question_text: q.question_text,
+		short_answer: q.short_answer,
+	}));
+
+	elems.previewTextArea.textContent = JSON.stringify(dump);
+	elems.status.textContent = "덤프 생성 완료. 다운로드 버튼을 누르세요.";
+	elems.downloadButton.disabled = false;
+
+	elems.downloadButton.addEventListener("click", () => downloadDump(subject));
+}
+
+async function fetchQuestions(subject: string): Promise<Question[]> {
+	const baseUrl = import.meta.env.PROD ? "" : "http://localhost:3000";
+	const res = await fetch(`${baseUrl}/questions?subjects=${subject}`);
+	const result = await res.json();
+
+	if (result.status === "error") {
+		const msg = result.error?.message || result.message || "Unknwon error";
+		throw new Error(`Failed to fetch question: ${msg}`);
+	}
+
+	if (result.data?.length === 0) {
+		throw new Error(`No questions found`);
+	}
+
+	return result.data as Question[];
+}
+
+// biome-ignore lint/suspicious/noExplicitAny: Promise.catch의 인자는 any 타입을 가짐.
+function renderError(reason: any) {
+	let msg: string;
+	if (reason instanceof Error) {
+		msg = reason.message;
+	} else {
+		msg = reason?.toString() || "unknown error";
+	}
+
+	elems.status.textContent = `오류: ${msg}`;
+}
+
+function downloadDump(subject: string) {
+	const filename = `poka-dump-${subject}.json`;
+	const dump = elems.previewTextArea.textContent;
+	const blob = new Blob([dump], { type: "application/json" });
+	const url = URL.createObjectURL(blob);
+
+	const link = document.createElement("a");
+	link.href = url;
+	link.download = filename;
+	link.style.display = "none";
+
+	document.body.appendChild(link);
+	link.click();
+
+	document.body.removeChild(link);
+	URL.revokeObjectURL(url);
+}
+
+main().catch(renderError);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,26 @@
 import { resolve } from "node:path";
-import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  root: "src",
-  build: {
-    rolldownOptions: {
-      input: {
-        main: resolve(import.meta.dirname, "src/index.html"),
-        editor: resolve(import.meta.dirname, "src/editor/index.html"),
-        testMarkdownEditor: resolve(import.meta.dirname, "src/editor/test-markdown-editor.html"),
-        print: resolve(import.meta.dirname, "src/print/index.html")
-      },
-    },
-    chunkSizeWarningLimit: 1000,
-    emptyOutDir: true,
-    outDir: "../dist",
-    assetsDir: "static",
-  },
-  plugins: [tailwindcss()],
+	root: "src",
+	build: {
+		rolldownOptions: {
+			input: {
+				main: resolve(import.meta.dirname, "src/index.html"),
+				editor: resolve(import.meta.dirname, "src/editor/index.html"),
+				testMarkdownEditor: resolve(
+					import.meta.dirname,
+					"src/editor/test-markdown-editor.html",
+				),
+				print: resolve(import.meta.dirname, "src/print/index.html"),
+				dump: resolve(import.meta.dirname, "src/dump/index.html"),
+			},
+		},
+		chunkSizeWarningLimit: 1000,
+		emptyOutDir: true,
+		outDir: "../dist",
+		assetsDir: "static",
+	},
+	plugins: [tailwindcss()],
 });
