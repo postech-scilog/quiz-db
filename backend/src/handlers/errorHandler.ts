292f9402c5a2cc0841df9a5d6778947dd8bc168a
Quasar-Kim <vhfflrhs128@gmail.com>
2026-06-01 04:07:48 +0900

    (backend) chore: styling

--- backend/src/handlers/errorHandler.ts
+++ backend/src/handlers/errorHandler.ts
@@ -2,29 +2,29 @@ import type { NextFunction, Request, Response } from "express";
 import { UserError } from "../error.ts";
 
 export default function errorHandler(
-  error: Error,
-  req: Request,
-  res: Response,
-  next: NextFunction,
+	error: Error,
+	req: Request,
+	res: Response,
+	next: NextFunction,
 ) {
-  // 이미 헤더가 전송된 경우 express.js 의 기본 오류 핸들러에게 처리를 넘김.
-  // 기본 오류 핸들러는 연결을 닫고 요청을 실패 처리함.
-  // (참조: https://expressjs.com/en/guide/error-handling.html 의 "The default error handler" 섹션)
-  if (res.headersSent) {
-    return next(error);
-  }
+	// 이미 헤더가 전송된 경우 express.js 의 기본 오류 핸들러에게 처리를 넘김.
+	// 기본 오류 핸들러는 연결을 닫고 요청을 실패 처리함.
+	// (참조: https://expressjs.com/en/guide/error-handling.html 의 "The default error handler" 섹션)
+	if (res.headersSent) {
+		return next(error);
+	}
 
-  req.log.error(error)
+	req.log.error(error);
 
-  if (error instanceof UserError) {
-    res.status(error.code).json({
-      status: "error",
-      error,
-    });
-  } else {
-    res.status(500).json({
-      status: "error",
-      message: "Unexpected internal error",
-    });
-  }
+	if (error instanceof UserError) {
+		res.status(error.code).json({
+			status: "error",
+			error,
+		});
+	} else {
+		res.status(500).json({
+			status: "error",
+			message: "Unexpected internal error",
+		});
+	}
 }
